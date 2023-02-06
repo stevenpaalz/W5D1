07fb630c7f9641a40138b9f065d8d28afac64718
Kevin Chan <chankevin13@gmail.com>
2023-02-06 15:27:42 -0500

    hashset

--- skeleton 2/lib/p03_hash_set.rb
+++ skeleton 2/lib/p03_hash_set.rb
@@ -7,17 +7,32 @@ class HashSet
   end
 
   def insert(key)
+    if @count = num_buckets
+      resize!
+    end
+
+    unless include?(key)
+      self[key] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if self[key].include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
+
   end
 
   private
 
   def [](num)
+    @store[num.hash % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -26,5 +41,12 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2){Array.new}
+    @store.each do |sub_arr|
+      sub_arr.each do |ele|
+        new_store[ele.hash % (num_buckets * 2)] << ele
+      end
+    end
+    @store = new_store
   end
 end
