2dd80850420368c93fa5c6ce63ac6d45540721fc
Steven Paalz <stevenpaalz@gmail.com>
2023-02-06 14:06:27 -0500

    IntSet

--- skeleton 2/lib/p01_int_set.rb
+++ skeleton 2/lib/p01_int_set.rb
@@ -32,23 +32,29 @@ end
 
 
 class IntSet
+  attr_reader :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num) 
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
