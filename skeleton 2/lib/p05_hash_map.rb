7f4490385a2061c760ef1ac1a88af26f11658281
Kevin Chan <chankevin13@gmail.com>
2023-02-06 17:14:20 -0500

    hash_map

--- skeleton 2/lib/p05_hash_map.rb
+++ skeleton 2/lib/p05_hash_map.rb
@@ -9,15 +9,29 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    if @count == num_buckets
+      resize!
+    end
+    unless bucket(key).include?(key)
+      bucket(key).append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).each do |node|
+      if node.key == key
+        return node
+      end
+    end
   end
 
   def delete(key)
+
   end
 
   def each
@@ -41,9 +55,17 @@ class HashMap
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2){LinkedList.new}
+    @store.each do |linked_list|
+      linked_list.each do |node|
+        new_store[node.key.hash % (num_buckets * 2)].append(node.key,node.val)
+      end
+    end
+    @store = new_store
   end
 
   def bucket(key)
+    @store[key.hash % num_buckets]
     # optional but useful; return the bucket corresponding to `key`
   end
 end
