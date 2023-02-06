fdb9e9375c39385d289e7615ce51597b6d1a6c11
Steven Paalz <stevenpaalz@gmail.com>
2023-02-06 15:01:58 -0500

    hashing a hash

--- skeleton 2/lib/p02_hashing.rb
+++ skeleton 2/lib/p02_hashing.rb
@@ -4,11 +4,19 @@ end
 
 class Array
   def hash
+    sum = 0
+    self.each_with_index do |ele, idx|
+      sum += (ele ^ idx).to_s(2).to_i * (10*idx)
+    end
+    sum.hash
   end
 end
 
 class String
   def hash
+    nums = []
+    self.split("").each {|char| nums += char.bytes}
+    nums.hash
   end
 end
 
@@ -16,6 +24,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    self.each do |k, v|
+      key_bytes = k.to_s.bytes
+      value_bytes = v.to_s.bytes
+      sum += (key_bytes[0] ^ value_bytes[0]).to_s(2).to_i.hash
+    end
+    sum
+    # 0
   end
 end
